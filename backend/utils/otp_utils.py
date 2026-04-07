a72e722f9f6f932350625da10206785a679bf17f
blackdevil0009 <black0009devil@gmail.com>
2026-04-07 19:50:44 +0530

    Remove backend directory

--- backend/utils/otp_utils.py
+++
@@ -1,6 +0,0 @@
-import secrets
-
-def generate_otp():
-    """Generates a secure 6-digit numeric OTP."""
-    # Using 'secrets' instead of 'random' for cryptographic security
-    return str(secrets.randbelow(900000) + 100000)
